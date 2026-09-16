68a847c1526d13774018797278a749df80c9d885
Twotronix <twotronix@gmail.com>
2026-09-16 19:49:59 +0100

    v1.1 - Added A Theme Change Depending On Which Way You Are Converting. Added A Catch To Tell The User If You Tried To Type In A Letter.  Made The Card Bigger On Ipads/Computers

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const convertBtn = document.querySelector("#convertBtn")
 const outputField = document.querySelector("#preview")
 const switchBtn = document.querySelector("#switch")
 const indicator = document.querySelector("#indicator")
+document.body.classList.toggle("normal")
 let way = true
 let started = false
 switchBtn.addEventListener("click", () => {
+    document.body.classList.toggle("secondary")
     if (way) {
         way = false
         indicator.textContent = "Converting °F To °C"
@@ -22,15 +24,21 @@ switchBtn.addEventListener("click", () => {
 function updateTemp() {
     if (way) {
         const text = (parseFloat(inputField.value) * 1.8) + 32
-            if (typeof text == "number" && !Number.isNaN(text)) { 
+            if (isNaN(text)) {
+                outputField.textContent = `Enter A Number!`
+                return
+            }
+            if (typeof text == "number" && !Number.isNaN(text.toFixed(2))) { 
                 outputField.textContent = `${text}°F`
             }
+
     }
     else {
         const text = (parseFloat(inputField.value) - 32) / 1.8
             if (typeof text == "number" && !Number.isNaN(text)) { 
                 outputField.textContent = `${text.toFixed(2)}°C`
             }
+
     }
 }
 
