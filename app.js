f8a49570ead89a9cf0c2c09faecff8a36e3cc024
Twotronix <twotronix@gmail.com>
2026-09-19 11:32:15 +0100

    changed the way it converts temperature by converting it to celcius first and then converting to desired temperature, making it easier to add more temperatures. added kelvin and rankine

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 const inputField = document.querySelector("#inputField")
 const convertBtn = document.querySelector("#convertBtn")
 const outputField = document.querySelector("#preview")
-const switchBtn = document.querySelector("#switch")
-const indicator = document.querySelector("#indicator")
 const drp1 = document.querySelector("#dropdown1")
 const drp2 = document.querySelector("#dropdown2")
 document.body.classList.toggle("normal")
 
-let started = false
-
 function updateTemp() {
     const drp1Val = drp1.value
     const drp2Val = drp2.value
-    const way = String(drp1Val + drp2Val)
-    if (way === "cf") {
-        const text = (parseFloat(inputField.value) * 1.8) + 32
-            if (isNaN(text)) {
-                outputField.textContent = `Enter A Number!`
-                return
-            }
-            if (typeof text == "number" && !Number.isNaN(text.toFixed(2))) { 
-                outputField.textContent = `${text}°F`
-            }
+    const startTemp = Number(inputField.value)
+    let celctemp
+    let finaltemp
+    if (isNaN(startTemp)) {
+        outputField.textContent = `Enter A Number`
+    }
+    if (!startTemp) {
+        outputField.textContent = `Enter A Number`
+    }
+    if (drp1Val === "c") {celctemp = startTemp}
+    else if (drp1Val === "f") {celctemp = (startTemp - 32) / 1.8}
+    else if (drp1Val === "k") {celctemp = startTemp - 273}
+    else if (drp1Val === "r") {celctemp = (startTemp - 491.67) / 1.8}
 
-    }
-    else if (way === "fc") {
-        const text = (parseFloat(inputField.value) - 32) / 1.8
-            if (typeof text == "number" && !Number.isNaN(text)) { 
-                outputField.textContent = `${text.toFixed(2)}°C`
-            }
+    if (drp2Val === "c") {finaltemp = celctemp}
+    else if (drp2Val == "f") {finaltemp = (celctemp * 1.8) + 32}
+    else if (drp2Val === "k") {finaltemp = celctemp + 273}
+    else if (drp2Val === "r") {finaltemp = (celctemp * 1.8) + 491.67}
 
-    }
-    else if (drp1Val === drp2Val) {
-        outputField.textContent = `Use Seperate Units`
-    }
+    outputField.textContent = `${finaltemp.toFixed(2)}°${drp2Val.toUpperCase()}`
 }
 
 convertBtn.addEventListener("click", () => {
